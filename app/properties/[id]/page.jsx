36b8502738416ecb8766c8f73f8eb8089a2e9f23
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-09 22:55:52 +0545

    learned some of the client side component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,19 @@
+'use client';
+import {useRouter,useParams,useSearchParams,usePathname} from 'next/navigation';
+
 const PropertyPage=()=>{
-    return<>PropertyPage</>
+    const router = useRouter();
+
+    const {id} = useParams;
+
+    // used for query search
+    const searchParams = useSearchParams();
+    const name = searchParams.get('name');
+
+    const pathName = usePathname();
+
+    return(
+        <button onClick={()=>router.push('/')} className='bg-blue-500 p-5'>Go Home {pathName}</button> 
+    )
 }
 export default PropertyPage;
