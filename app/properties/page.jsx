40f293dcc870a25f85a2d501d5b34de9d9c4a8af
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-09 22:12:22 +0545

    package installed to make the tailwindcss work

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 
-const PropertiesPage = ()=>{
+const PropertiesPage = () => {
     return (
         <div>
             <h1 className="text-3xl">These are the properties</h1>
-            <Link href='/'>Back to home</Link>
+            <Link href='/' className="no-underline text-blue-600 hover:underline">
+                Back to home
+            </Link>
+
+
         </div>
     )
 }
