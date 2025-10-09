94465e4e702296799f96963edef8838e8af4583d
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-09 23:10:06 +0545

    removed the prev code

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,19 +1,8 @@
 'use client';
-import {useRouter,useParams,useSearchParams,usePathname} from 'next/navigation';
 
 const PropertyPage=()=>{
-    const router = useRouter();
-
-    const {id} = useParams;
-
-    // used for query search
-    const searchParams = useSearchParams();
-    const name = searchParams.get('name');
-
-    const pathName = usePathname();
-
     return(
-        <button onClick={()=>router.push('/')} className='bg-blue-500 p-5'>Go Home {pathName}</button> 
+        <div>PropertyPage</div>
     )
 }
 export default PropertyPage;
