ad5d7b17813ab78b9d4de15f42110def5f5fcd77
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-19 23:00:22 +0545

    changed the location of loading to main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
 import Hero from '@/components/Hero.jsx';
 import InfoBoxes from '@/components/InfoBoxes.jsx';
-import Footer from '@/components/Footer';
 
 const HomePage = () => {
     return (
         <>
           <Hero/>
           <InfoBoxes/>
-          <Footer/>
         </>
     )
 };
