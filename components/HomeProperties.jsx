aab114477c24e38d7dcf0313d2c34d76e5cd768f
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-20 17:41:50 +0545

    completed the not found page

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -28,7 +28,7 @@ const HomeProperties = ({ }) => {
             <section className="m-auto max-w-lg my-10 px-6">
                 <Link
                     href="/properties"
-                    class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+                    className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
                 >View All Properties
                 </Link>
             </section>
