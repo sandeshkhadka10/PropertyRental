75c2062041b6a6581aca7cef67890b3ed4229cd9
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-20 14:07:34 +0545

    working over the properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,15 +1,18 @@
-import Link from "next/link";
+import properties from '@/properties.json';
 
 const PropertiesPage = () => {
     return (
-        <div>
-            <h1 className="text-3xl">These are the properties</h1>
-            <Link href='/'>
-                Back to home
-            </Link>
-
-
-        </div>
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">\
+                    {properties.map((property)=>(
+                        <div>
+                            {property.name}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
 
