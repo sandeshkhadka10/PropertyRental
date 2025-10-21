484cb7f52a7e0039d1b0ea76572b1e9c60a71afd
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-21 17:56:27 +0545

    sorted the properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -14,6 +14,10 @@ async function fetchProperties(){
 
 const PropertiesPage = async() => {
     const properties = await fetchProperties();
+
+    // sort properties by date
+    properties.sort((a,b)=>new Date(b.createdAt) - new Date(a.createdAt));
+    
     return (
         <section className="px-4 py-6">
             <div className="container-xl lg:container m-auto px-4 py-6">
