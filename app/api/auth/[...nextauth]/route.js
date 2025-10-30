a8142137f671a74ab366850531d2ca5959444a58
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-10-30 18:35:12 +0545

    comment added to make it understandable

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,10 @@
 import {authOptions} from '@/utils/authOptions.js';
 import NextAuth from 'next-auth';
 
+// Create NextAuth API handler using your custom options
 const handler = NextAuth(authOptions);
 
+// Export for GET and POST methods — Next.js will call them automatically
 export {handler as GET, handler as POST};
+
+// So whenever a request hits /api/auth/..., NextAuth handles it automatically.
