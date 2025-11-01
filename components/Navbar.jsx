f05bf028f5529b1d81202b69dc3cc3506c4c4fe6
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-01 19:08:20 +0545

    added the middleware and state function

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -196,6 +196,9 @@ const Navbar = () => {
                                             role='menuitem'
                                             tabIndex='-1'
                                             id='user-menu-item-0'
+                                            onClick={()=>{
+                                                setIsProfileMenuOpen(false);
+                                            }}
                                         >
                                             Your Profile
                                         </Link>
@@ -205,6 +208,9 @@ const Navbar = () => {
                                             role='menuitem'
                                             tabIndex='-1'
                                             id='user-menu-item-2'
+                                            onClick={()=>{
+                                                setIsProfileMenuOpen(false);
+                                            }}
                                         >
                                             Saved Properties
                                         </Link>
