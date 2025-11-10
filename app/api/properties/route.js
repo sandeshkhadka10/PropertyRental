742988f43d662c9c982cfb110bfadf95b34c75c6
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-10 08:36:09 +0545

    testing whether data is sent from frontend to backend

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -26,7 +26,36 @@ export const POST = async (request)=>{
         // ignore empty or invalid file inputs for images and accept the one in array
         const images = formData.getAll('images').filter((image)=>image.name !== '');
 
-        console.log(amenities,images);
+        // console.log(amenities,images);
+
+        const propertyData = {
+            type:formData.get('type'),
+            name:formData.get('name'),
+            description:formData.get('description'),
+            location:{
+                street:formData.get('location.street'),
+                city:formData.get('location.city'),
+                state:formData.get('location.state'),
+                zipcode:formData.get('location.zipcode'),
+            },
+            beds:formData.get('beds'),
+            baths:formData.get('baths'),
+            square_feet:formData.get('square_feet'),
+            amenities,
+            rates:{
+                weekly:formData.get('rates.weekly'),
+                monthly:formData.get('rates.monthly'),
+                nightly:formData.get('rates.nightly')
+            },
+            seller_info:{
+                name:formData.get('seller_info.name'),
+                email:formData.get('seller_info.email'),
+                phone:formData.get('seller_info.phone')
+            },
+            images
+        };
+
+        console.log(propertyData);
 
         return new Response(JSON.stringify({message:"Successfull"}),{status:200});
     }catch(error){
