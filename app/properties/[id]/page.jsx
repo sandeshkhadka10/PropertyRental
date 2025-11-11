4a70e584e16df79b535eca8bb4301d35ad6b2a2e
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-12 00:15:45 +0545

    successfully fetched the image

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import PropertyDetails from '@/components/PropertyDetails';
 import {FaArrowLeft} from 'react-icons/fa';
 import Spinner from '@/components/Spinner';
+import PropertyImages from '@/components/PropertyImages';
 
 const PropertyPage = () => {
     const { id } = useParams();
@@ -155,7 +156,9 @@ const PropertyPage = () => {
                             </div>
                         </div>
                     </section>
-                </>)}
+                    <PropertyImages images={property.images}/>
+                </>
+            )}
         </>
     )
 }
