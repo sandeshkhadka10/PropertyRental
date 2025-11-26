9b95119d6fce816b7d5a820f8f4128533dc3dcdd
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-26 09:13:32 +0545

    updated and seperated the share component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -9,6 +9,7 @@ import {FaArrowLeft} from 'react-icons/fa';
 import Spinner from '@/components/Spinner';
 import PropertyImages from '@/components/PropertyImages';
 import BookMarkButton from '@/components/BookmarkButton';
+import ShareButtons from '@/components/ShareButtons';
 
 const PropertyPage = () => {
     const { id } = useParams();
@@ -72,12 +73,8 @@ const PropertyPage = () => {
                                 {/* <!-- Sidebar --> */}
                                 <aside className="space-y-4">
                                     <BookMarkButton property={property}/>
-                                    <button
-                                        className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-                                    >
-                                        <i className="fas fa-share mr-2"></i> Share Property
-                                    </button>
-
+                                    <ShareButtons property={property}/>
+                                    
                                     {/* <!-- Contact Form --> */}
                                     <div className="bg-white p-6 rounded-lg shadow-md">
                                         <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
