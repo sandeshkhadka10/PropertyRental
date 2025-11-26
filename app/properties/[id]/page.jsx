efaae15a1e764dc0df1cfa1d41abd1b9ed46f11c
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-26 09:09:36 +0545

    seperated the bookmark component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -8,6 +8,7 @@ import PropertyDetails from '@/components/PropertyDetails';
 import {FaArrowLeft} from 'react-icons/fa';
 import Spinner from '@/components/Spinner';
 import PropertyImages from '@/components/PropertyImages';
+import BookMarkButton from '@/components/BookmarkButton';
 
 const PropertyPage = () => {
     const { id } = useParams();
@@ -70,11 +71,7 @@ const PropertyPage = () => {
 
                                 {/* <!-- Sidebar --> */}
                                 <aside className="space-y-4">
-                                    <button
-                                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-                                    >
-                                        <i className="fas fa-bookmark mr-2"></i> Bookmark Property
-                                    </button>
+                                    <BookMarkButton property={property}/>
                                     <button
                                         className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
                                     >
