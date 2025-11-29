2100cceeae1e3fa22aae059c5ca91c555ac7f8e8
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-29 16:10:01 +0545

    notification count started working

---
+++ components/UnreadMessageCount.jsx
@@ -0,0 +1,33 @@
+'use client';
+import {useState,useEffect} from 'react';
+
+const UnreadMessageCount = ({session}) => {
+    const [unreadMessageCount,setUnreadMessageCount] = useState(0);
+
+    useEffect(()=>{
+        if(!session){
+            return;
+        }
+        const fetchUnreadMessages = async()=>{
+            try{
+                const res = await fetch('/api/messages/unread-count');
+                if(res.status === 200){
+                    const data = await res.json();
+                    setUnreadMessageCount(data);
+                }
+            }catch(error){
+                console.log(error);
+            }
+        }
+        fetchUnreadMessages();
+    },[session]);
+
+    return unreadMessageCount > 0 && (
+        <span className='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
+            {unreadMessageCount}
+            {/* <!-- Replace with the actual number of notifications --> */}
+        </span>
+    )
+}
+
+export default UnreadMessageCount;
