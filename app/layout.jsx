1ade9f09033ef761d6aa356d23302abbd50ad254
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-30 22:23:47 +0545

    made the footer sticky

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,10 +18,15 @@ const MainLayout = ({ children }) => {
         <GlobalProvider>
             <AuthProvider>
                 <html lang="en">
-                    <body>
+                    <body className="min-h-screen flex flex-col">
                         <Navbar />
-                        <main>{children}</main>
+
+                        <main className="flex-grow">
+                            {children}
+                        </main>
+
                         <Footer />
+
                         <ToastContainer />
                     </body>
                 </html>
