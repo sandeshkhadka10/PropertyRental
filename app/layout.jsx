e90cadefadaf68e4121ef68eb04e819532581089
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-30 18:06:11 +0545

    implated the package to swipe the images

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import AuthProvider from '@/components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { GlobalProvider } from '@/context/GlobalContext';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
     title: 'PropertyPlus | Find The Perfect |Rental',
