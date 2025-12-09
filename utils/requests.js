8eea31f76d2987bf136d409e50b1c1bc8fb4d476
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-12-09 21:08:46 +0545

    small update in fetching all properties

--- utils/requests.js
+++ utils/requests.js
@@ -1,18 +1,13 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
-async function fetchProperties({ showFeatured = false } = {}) {
+async function fetchProperties({showFeatured=false}={}) {
   try {
     // handle the case where domain is not available yet
     if (!apiDomain) {
       return [];
     }
-    const res = await fetch(
-      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`,
-      {
-        next: { revalidate: 60 },
-      }
-    );
+    const res = await fetch(`${apiDomain}/properties${showFeatured?'/featured':''}`,{cache:'no-store'});
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
@@ -41,4 +36,4 @@ async function fetchProperty(id) {
   }
 }
 
-export { fetchProperties, fetchProperty };
+export { fetchProperties,fetchProperty };
