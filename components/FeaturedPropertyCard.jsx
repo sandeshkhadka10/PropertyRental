8cc36ca42e3effcdd2f8b309c346dec472186891
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-09-04 18:56:51 +0545

    changed dollar sign into nepali rupee sign

--- components/FeaturedPropertyCard.jsx
+++ components/FeaturedPropertyCard.jsx
@@ -38,7 +38,7 @@ const FeaturedPropertyCard = ({ property }) => {
                 <h3
                     className="absolute top-[10px] left-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right"
                 >
-                    ${getRateDisplay()}
+                    ₨{getRateDisplay()}
                 </h3>
                 <div className="flex justify-center gap-4 text-gray-500 mb-4">
                     <p>
--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -34,7 +34,7 @@ const PropertyDetails = ({ property }) => {
                     >
                         <div className="text-gray-500 mr-2 font-bold">Nightly</div>
                         <div className="text-2xl font-bold text-blue-500">
-                            {property.rates.nightly ? (`$${property.rates.nightly.toLocaleString()}`) : (<FaTimes className='text-red-700' />)}
+                            {property.rates.nightly ? (`₨${property.rates.nightly.toLocaleString()}`) : (<FaTimes className='text-red-700' />)}
                         </div>
                     </div>
                     <div
@@ -42,13 +42,13 @@ const PropertyDetails = ({ property }) => {
                     >
                         <div className="text-gray-500 mr-2 font-bold">Weekly</div>
                         <div className="text-2xl font-bold text-blue-500">
-                            {property.rates.weekly ? (`$${property.rates.weekly.toLocaleString()}`) : (<FaTimes className='text-red-700' />)}
+                            {property.rates.weekly ? (`₨${property.rates.weekly.toLocaleString()}`) : (<FaTimes className='text-red-700' />)}
                         </div>
                     </div>
                     <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
                         <div className="text-gray-500 mr-2 font-bold">Monthly</div>
                         <div className="text-2xl font-bold text-blue-500">
-                            {property.rates.monthly ? (`$${property.rates.monthly.toLocaleString()}`) : (<FaTimes className='text-red-700' />)}
+                            {property.rates.monthly ? (`₨${property.rates.monthly.toLocaleString()}`) : (<FaTimes className='text-red-700' />)}
                         </div>
                     </div>
                 </div>
